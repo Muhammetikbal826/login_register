0c675fe5f6caf993f4e09420aea2912a34772602
muhammed ikbal aşık <muhammetikbal826@gmail.com>
2022-11-04 19:02:46 +0300

    login_register
    laravel ile login ,register kısmı

---
+++ app/Http/Controllers/Samplecontroller.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class SampleController extends Controller
+{
+    function index()
+    {
+        return view('login');
+    }
+
+    function registration()
+    {
+        return view('registration');
+    }
+
+    function validate_registration(Request $request)
+    {
+        $request->validate([
+            'name'         =>   'required',
+            'email'        =>   'required|email|unique:users',
+            'password'     =>   'required|min:6'
+        ]);
+
+        $data = $request->all();
+
+        User::create([
+            'name'  =>  $data['name'],
+            'email' =>  $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        return redirect('login')->with('success', 'Registration Completed, now you can login');
+    }
+
+    function validate_login(Request $request)
+    {
+        $request->validate([
+            'email' =>  'required',
+            'password'  =>  'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if(Auth::attempt($credentials))
+        {
+            return redirect('dashboard');
+        }
+
+        return redirect('login')->with('success', 'Login details are not valid');
+    }
+
+    function dashboard()
+    {
+        if(Auth::check())
+        {
+            return view('dashboard');
+        }
+
+        return redirect('login')->with('success', 'you are not allowed to access');
+    }
+
+    function logout()
+    {
+        Session::flush();
+
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('main')
+
+@section('content')
+
+<div class="card">
+	<div class="card-header">Dashboard</div>
+	<div class="card-body">
+		
+		You are Login in Laravel 9 Custom Login Registration Application.
+	</div>
+</div>
+
+@endsection('content')
